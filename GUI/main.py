b402621f305061fdac874b51598b2df9b801b433
DhimanGhosh <dgkiitcsedual@gmail.com>
2020-03-28 02:11:21 +0530

    Updated GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,26 +1,19 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.dropdown import DropDown
 from kivy.properties import ObjectProperty
 
-class MyGrid(Widget):
-    fname = ObjectProperty(None)
-    lname = ObjectProperty(None)
-    email = ObjectProperty(None)
-
-    def btn(self):
-        print("First Name: {}\nLast Name: {}\nEmail: {}".format(self.fname.text, self.lname.text, self.email.text))
-        self.fname.text = ""
-        self.lname.text = ""
-        self.email.text = ""
+class MyLayout(Widget):
+    spinner_vals = ['Major Scale', 'Major Chord', 'Notes in Major Scale', 'Note in Major Scales', 'Note shift with capo position (Guitar)', 'Scale shift with capo position (Guitar)', 'Minor Scale', 'Minor Chord', 'Notes in Minor Scale', 'Relative Minor/Major', 'Play Tone Based on Note', 'Play Tone in Sequence (Scale / Note Sequence)', 'Scale from Chords']
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return MyLayout()
 
 if __name__ == "__main__":
     MyApp().run()

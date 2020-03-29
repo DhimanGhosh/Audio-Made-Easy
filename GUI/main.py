368cd4ef1f158ffb41a4fbcdffec470bea2f4b7b
DhimanGhosh <dgkiitcsedual@gmail.com>
2020-03-29 19:21:21 +0530

    Updated GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,15 +1,12 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.dropdown import DropDown
 from kivy.properties import ObjectProperty
 
 class MyLayout(Widget):
     spinner_vals = ['Major Scale', 'Major Chord', 'Notes in Major Scale', 'Note in Major Scales', 'Note shift with capo position (Guitar)', 'Scale shift with capo position (Guitar)', 'Minor Scale', 'Minor Chord', 'Notes in Minor Scale', 'Relative Minor/Major', 'Play Tone Based on Note', 'Play Tone in Sequence (Scale / Note Sequence)', 'Scale from Chords']
+    notesS = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+    notesb = ['A','Bb','Cb','C','Db','D','Eb','Fb','F','Gb','G','Ab']
 
 class MyApp(App):
     def build(self):

0c6815c79a970be63c34375ab9766c049fd945b7
DhimanGhosh <dgkiitcsedual@gmail.com>
2020-04-01 19:43:16 +0530

    Added One Click Installation for Windows / Linux

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,7 +5,7 @@ case "$OSTYPE" in
 		sudo apt-get -y install python3-kivy
 		pip install -r requirements.txt ;;
     msys*)
-		pip install --upgrade -r upgrade_kivy.txt
+		pip install --upgrade pip wheel setuptools virtualenv
 		pip install kivy_deps.sdl2==0.1.*
 		pip install kivy_deps.glew==0.1.*
 		pip install kivy_deps.gstreamer==0.1.*

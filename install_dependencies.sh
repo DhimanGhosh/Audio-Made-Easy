e11e25fb365f36446c46d9f116a42ac339e59b23
DhimanGhosh <dgkiitcsedual@gmail.com>
2020-04-01 19:38:42 +0530

    Added One Click Installation for Windows / Linux

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,16 +1,15 @@
 case "$OSTYPE" in
     linux*)
-	sudo add-apt-repository -y ppa:kivy-team/kivy
-	sudo apt-get -y update
-	sudo apt-get -y install python3-kivy
-	pip install -r requirements.txt ;;
+		sudo add-apt-repository -y ppa:kivy-team/kivy
+		sudo apt-get -y update
+		sudo apt-get -y install python3-kivy
+		pip install -r requirements.txt ;;
     msys*)
-	pip install --upgrade -r upgrade_kivy.txt
-	pip install kivy_deps.sdl2==0.1.*
-	pip install kivy_deps.glew==0.1.*
-	pip install kivy_deps.gstreamer==0.1.*
-	pip install kivy_deps.angle==0.1.*
-	pip install kivy==1.11.1
-	pip install -r requirements.txt ;;
+		pip install --upgrade -r upgrade_kivy.txt
+		pip install kivy_deps.sdl2==0.1.*
+		pip install kivy_deps.glew==0.1.*
+		pip install kivy_deps.gstreamer==0.1.*
+		pip install kivy_deps.angle==0.1.*
+		pip install kivy==1.11.1
+		pip install -r requirements.txt ;;
 esac
-
